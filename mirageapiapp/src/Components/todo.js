d3072cea0cb26472772c9c46354ff05f489b1969
Farrukh <farrukhkhan30@hotmail.com>
2020-09-05 21:02:32 +0500

    initial commit

--- mirageapiapp/src/Components/todo.js
+++ mirageapiapp/src/Components/todo.js
@@ -1,15 +1,43 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 
-let baseUrl='fakeapi'
-useEffect(()=>{
-    fetch(`/${baseUrl}/getTodo`)
-    .then(res=>res.json)
-    .then(data=>console.log(data))
-})
+
+
+let baseUrl = 'fakeapi'
+
 const Todo = () => {
+    const [datas,setData]=useState({})
+    // useEffect(() => {
+    //     fetch(`/${baseUrl}/getTodo`)
+    //         .then(res => res.json)
+    //         .then(data => console.log(data))
+    // })
+
+    const addTodoHandler = () => {
+        fetch(`/${baseUrl}/addTodo`, {
+            method: 'POST',
+            body: { 'id': 3, 'text': 'Farrukh Khan' }
+        })
+            .then(res => console.log("SUCCESS", res))
+            .catch(error => console.log('Error', error))
+    }
+
+    const getDataHandlers=()=>{
+        fetch(`/${baseUrl}/getTodo`)
+        .then(res => res.json)
+        .then(data =>{setData(data.todo)
+                console.log(data)        
+        }  
+            
+            )
+    }
     return (
         <div>
-            
+            Hello From Todo
+            <button onClick={addTodoHandler}>ADD TODO</button>
+            <button onClick={getDataHandlers}>GET TODO</button>
+            {
+                JSON.stringify(datas)
+            }
         </div>
     )
 }
--- mirageapiapp/src/Mirage/index.js
+++ mirageapiapp/src/Mirage/index.js
@@ -4,8 +4,13 @@ export default function () {
     let arr=[{'id':1,'text':'Hello Boy'}]
   createServer({
     routes() {
-        this.namespace='/fakeapi'
+      this.namespace='/fakeapi'
       this.get("/getTodo",{todo:arr})
+
+      this.post("/addTodo",(_,req)=>{
+          console.log('Request',req)
+          req.push(req.requestBody)
+      })
     },
   })
 }
